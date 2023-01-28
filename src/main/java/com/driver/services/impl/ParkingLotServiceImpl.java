d31e26d8961eaa13485644973f204f755cbde0f2
Hitesh <gaikwadhitesh03@gmail.com>
2023-01-29 01:02:29 +0530

    new chng5

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -39,9 +39,9 @@ public class ParkingLotServiceImpl implements ParkingLotService {
         spot.setPricePerHour(pricePerHour);
         spot.setOccupied(false);
 
-        if(numberOfWheels<=2){
+        if(numberOfWheels<4){
             spot.setSpotType(SpotType.TWO_WHEELER);
-        }else if(numberOfWheels<=4){
+        }else if(numberOfWheels<5){
             spot.setSpotType(SpotType.FOUR_WHEELER);
         }else{
             spot.setSpotType(SpotType.OTHERS);
